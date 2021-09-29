6554b016f1cfa1ad813a2db19f5c9d1eec7c30c8
iqraislamaddina <90311861+iqraislamaddina@users.noreply.github.com>
2021-09-29 14:23:10 +0700

    Feat/tutorial 3 pergipergi (#6)
    * feat: implementasi pergipergi
    
    * Update README.md
    
    Co-authored-by: qracapek <kingkinkane01@gmail.com>

--- pergipergi/src/main/java/apap/tutorial/pergipergi/controller/TravelAgensiController.java
+++ pergipergi/src/main/java/apap/tutorial/pergipergi/controller/TravelAgensiController.java
@@ -1,115 +1,103 @@
 package apap.tutorial.pergipergi.controller;
-
+ 
+import apap.tutorial.pergipergi.model.TourGuideModel;
 import apap.tutorial.pergipergi.model.TravelAgensiModel;
 import apap.tutorial.pergipergi.service.TravelAgensiService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalTime;
+import java.util.ArrayList;
 import java.util.List;
-
+ 
 @Controller
 public class TravelAgensiController {
-   @Autowired
-   private TravelAgensiService travelAgensiService;
 
-   //Routing URL yang diinginkan
-   @RequestMapping("agensi/add")
-   public String addAgensi(
-           @RequestParam(value = "idAgensi", required = true) String idAgensi,
-           @RequestParam(value = "namaAgensi", required = true) String namaAgensi,
-           @RequestParam(value = "alamat", required = true) String alamat,
-           @RequestParam(value = "noTelepon", required = true) String noTelepon,
-           Model model
-           ){
+    @Qualifier("travelAgensiServiceImpl")
+    @Autowired
+    private TravelAgensiService travelAgensiService;
 
-           //Membuat objek TravelAgensiModel
-           TravelAgensiModel agensi = new TravelAgensiModel(idAgensi, namaAgensi, alamat, noTelepon);
-
-           //Memanggil Servis addAgensi
-           travelAgensiService.addAgensi(agensi);
-
-           //Add variabel id agensi ke 'idAgensi' untuk dirender di thymeleaf
-           model.addAttribute("idAgensi", idAgensi);
-
-           //Add variabel id agensi ke 'idAgensi' untuk dirender di thymeleaf
-           model.addAttribute("idAgensi", idAgensi);
-
-           //return view template yang digunakan
-           return "add-agensi";
-   }
-   @RequestMapping("agensi/viewAll")
-   public String listAgensi (Model model) {
-
-       //mendapatkan semua TravelAgensiModel
-       List<TravelAgensiModel> listAgensi = travelAgensiService.getListAgensi();
-
-       //Add variabel semua TravelAgensiModel ke "list Agensi" untuk dirender pada thymeleaf
-       model.addAttribute("listAgensi", listAgensi);
-
-       //Return view template yang diinginkan
-       return "viewall-agensi";
-   }
-
-   @RequestMapping("agensi/view")
-   public String detailAgensi(
-           @RequestParam(value = "idAgensi") String idAgensi,
-           Model model
-   ){
-
-       //Mendapatkan TravelAgensiModel sesuai dengan idAgensi
-       TravelAgensiModel agensi = travelAgensiService.getAgensiByidAgensi(idAgensi);
-
-       //Add variabel TravelAgensiModel ke "agensi" untuk dirender pada thymeleaf
-       model.addAttribute("agensi", agensi);
-
-       return "view-agensi";
-   }
-
-   @GetMapping("/agensi/view/id-agensi/{idAgensi}")
-    public String viewAgensi(@PathVariable("idAgensi") String idAgensi, Model model){
-
-   //Mendapatkan TravelAgensiModel sesuai dengan idAgensi
-   TravelAgensiModel agensi = travelAgensiService.getAgensiByidAgensi(idAgensi);
-
-   //Add variabel TravelAgensiModel ke "agensi" untuk dirender pada thymeleaf
-   model.addAttribute("agensi", agensi);
-
-   return "view-agensi";
-}
-
-@GetMapping("/agensi/update/id-agensi/{idAgensi}/no-telepon/{noTelepon}")
-public String updateAgensi(
-       @PathVariable("idAgensi") String idAgensi,
-       @PathVariable("noTelepon") String noTelepon,
-       Model model
-){
-   TravelAgensiModel agensi = travelAgensiService.getAgensiByidAgensi(idAgensi);
-   agensi.setNoTelepon(noTelepon);
-   model.addAttribute("agensi", agensi);
-
-
-   //return view template yang digunakan
-   return "view-agensi";
-}
-
-@GetMapping("/agensi/delete/id-agensi/{idAgensi}")
-public String deleteAgensi(
-    @PathVariable("idAgensi") String idAgensi,
-    Model model){
-
-   TravelAgensiModel agensi = travelAgensiService.getAgensiByidAgensi(idAgensi);
-   if (agensi != null){
-    travelAgensiService.deleteAgensi(agensi);
-    model.addAttribute("agensi", agensi);
-    return "delete-agensi";
+    @GetMapping("/agensi/add")
+    public String addAgensiFormPage(Model model) {
+        model.addAttribute("agensi", new TravelAgensiModel());
+        return "form-add-agensi";
     }
 
-    return "no-agency";
-}
+    @PostMapping("/agensi/add")
+    public String addAgensiSubmitPage(
+        @ModelAttribute TravelAgensiModel agensi,
+        Model model 
+    ){
+        travelAgensiService.addAgensi(agensi);
+        model.addAttribute("noAgensi", agensi.getNoAgensi());
+        return "add-agensi";
+    }
+
+    @GetMapping("/agensi/viewall")
+    public String listAgensi(Model model){
+        List<TravelAgensiModel> listAgensi = travelAgensiService.getListAgensi();
+        model.addAttribute("listAgensi", listAgensi);
+        return "viewall-agensi";
+    }
+
+    @GetMapping("/agensi/view")
+    public String viewDetailAgensiPage(
+        @RequestParam(value = "noAgensi") Long noAgensi,
+        Model model
+    ){
+        TravelAgensiModel agensi = travelAgensiService.getAgensiBynoAgensi(noAgensi);
+        List<TourGuideModel> listTourGuide = agensi.getListTourGuide();
+
+        model.addAttribute("agensi", agensi);
+        model.addAttribute("listTourGuide", listTourGuide);
+        return "view-agensi";
+    }
+@GetMapping("/agensi/update/{noAgensi}")
+    public String updateAgensiFormPage(
+        @PathVariable Long noAgensi,
+        Model model
+        ){
+            TravelAgensiModel agensi = travelAgensiService.getAgensiBynoAgensi(noAgensi);
+            model.addAttribute("agensi", agensi);
+            return "form-update-agensi";
+        }
+
+    @PostMapping("/agensi/update")
+    public String updateAgensiSubmitPage(
+        @ModelAttribute TravelAgensiModel agensi,
+        Model model
+    ){
+        TravelAgensiModel updatedAgensi = travelAgensiService.updateAgensi(agensi);
+        model.addAttribute("noAgensi", updatedAgensi.getNoAgensi());
+        return "update-agensi";
+    }
+    
+    @GetMapping("/agensi/delete/{noAgensi}")
+        public String deleteTravelAgensiPage(
+            @PathVariable Long noAgensi,
+            Model model
+        ){
+            TravelAgensiModel agensi = travelAgensiService.getAgensiBynoAgensi(noAgensi);
+            List<TourGuideModel> list_tour = agensi.getListTourGuide();
+            String nama = agensi.getNamaAgensi();
+            LocalTime DateNow = LocalTime.now();
+            // print result
+            System.out.println("LocalDateTime : "+ DateNow);
+            boolean sebelumbuka = DateNow.isBefore(agensi.getWaktuBuka());
+            boolean setelahtutup = DateNow.isAfter(agensi.getWaktuTutup());
+            boolean bukatutup = agensi.getWaktuBuka().isBefore(agensi.getWaktuTutup());
+            if (((bukatutup && (sebelumbuka || setelahtutup)) || (!bukatutup && sebelumbuka && setelahtutup)) && list_tour.isEmpty()){
+                travelAgensiService.deleteAgensiBynoAgensi(noAgensi);
+                model.addAttribute("victory_message", "Agensi " + nama +" berhasil dihapus!");
+                return "home";
+            } else {
+                model.addAttribute("error_message", "Tidak bisa di hapus sebelum tutup atau tour guide kosong!");
+                return "error";
+            }
+            
+        }
 
 }
--- pergipergi/src/main/java/apap/tutorial/pergipergi/model/TravelAgensiModel.java
+++ pergipergi/src/main/java/apap/tutorial/pergipergi/model/TravelAgensiModel.java
@@ -1,40 +1,62 @@
 package apap.tutorial.pergipergi.model;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.io.Serializable;
+import java.time.LocalTime;
+import java.util.List;
 
-public class TravelAgensiModel {
-    private String idAgensi;
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@Entity
+@Table(name = "travel_agensi")
+
+public class TravelAgensiModel implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long noAgensi;
+
+    @NotNull
+    @Size(max=30)
+    @Column(name="nama_agensi", nullable = false)
     private String namaAgensi;
-    private String alamat;
-    private String noTelepon;
-    
-    public TravelAgensiModel(String idAgensi, String namaAgensi, String alamat, String noTelepon) {
-        this.idAgensi = idAgensi;
-        this.namaAgensi = namaAgensi;
-        this.alamat = alamat;
-        this.noTelepon = noTelepon;
-    }
-    public String getIdAgensi() {
-        return idAgensi;
-    }
-    public String getNoTelepon() {
-        return noTelepon;
-    }
-    public void setNoTelepon(String noTelepon) {
-        this.noTelepon = noTelepon;
-    }
-    public String getAlamat() {
-        return alamat;
-    }
-    public void setAlamat(String alamat) {
-        this.alamat = alamat;
-    }
-    public String getNamaAgensi() {
-        return namaAgensi;
-    }
-    public void setNamaAgensi(String namaAgensi) {
-        this.namaAgensi = namaAgensi;
-    }
-    public void setIdAgensi(String idAgensi) {
-        this.idAgensi = idAgensi;
-    }
-       
+
+    @NotNull
+    @Size(max=30)
+    @Column(name="alamat_agensi", nullable = false)
+    private String alamatAgensi;
+
+    @NotNull
+    @Size(max=30)
+    @Column(name="no_telepon_aqensi", nullable = false)
+    private String noTeleponAgensi;
+
+    @NotNull
+    @Column(nullable = false)
+    @DateTimeFormat(pattern= "HH:mm")
+    private LocalTime waktuBuka;
+
+    @NotNull
+    @Column(nullable = false)
+    @DateTimeFormat(pattern = "HH:mm")
+    private LocalTime waktuTutup;
+
+    //Relasi dengan TourGuideModel
+    @OneToMany(mappedBy = "agensi", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private List<TourGuideModel> listTourGuide;
+
+    //Relasi dengan DestinasiModel
+    @ManyToMany
+    @JoinTable(
+        name = "agensi_destinasi",
+        joinColumns = @JoinColumn(name = "no_agensi"),
+        inverseJoinColumns = @JoinColumn(name = "no_destinasi"))
+    List<DestinasiModel> listDestinasi;
 }
